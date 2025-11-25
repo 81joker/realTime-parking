9b4ccd0c9dbaebe65cb56bf485ffe95111be74b1
joker81 <nehad.al.timimi@gmail.com>
2025-11-25 14:39:32 +0100

    Refactor UserController and update route file for consistency

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class UserController extends Controller
 {
     public function __invoke()
     {
         // Logic to handle the /users route
-        return "Users"; // Assuming you have a users/index.blade.php view
+        return 'Users'; // Assuming you have a users/index.blade.php view
     }
 }
