d436be52d5df6c1d6f5cad68c43815dc3a511f80
joker81 <nehad.al.timimi@gmail.com>
2025-11-25 14:48:20 +0100

    change the name controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-    public function __invoke()
+    public function index()
     {
         // Logic to handle the /users route
         return 'Users'; // Assuming you have a users/index.blade.php view
