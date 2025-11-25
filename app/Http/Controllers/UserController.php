d4debb01a3dc76a43d40b176cace92dc9960f4b5
joker81 <nehad.al.timimi@gmail.com>
2025-11-25 14:35:47 +0100

    Implement user route handling in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        // Logic to handle the /users route
+        return "Users"; // Assuming you have a users/index.blade.php view
+    }
 }
