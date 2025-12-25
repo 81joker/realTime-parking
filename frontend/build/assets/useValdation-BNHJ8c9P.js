f92bfede6308fdadc422f4d6693c31216f012874
Nehad <35563834+81joker@users.noreply.github.com>
2025-12-25 10:02:31 +0100

    Add useValidation component for error handling in frontend

---
+++ frontend/build/assets/useValdation-BNHJ8c9P.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-y9liIk2y.js";function i(e,r){return t.jsx(t.Fragment,{children:e?.[r]?.map((a,n)=>t.jsx("div",{className:"text-danger mt-1 fw-bold",children:a},n))})}export{i as u};
